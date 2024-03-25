91308cd65018a8976c5dad04d7ac7887498e13ee
JoseBambora <zebragapt@gmail.com>
2024-03-25 14:22:40 +0000

    Added and applied some changes to the open source scrapper https://github.com/ShayneEvans/csstats_scraper from csstats website.

---
+++ CS-API/csgostats_scraper.py
@@ -0,0 +1,86 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import re
+from selenium.common.exceptions import NoSuchElementException, JavascriptException
+op = webdriver.ChromeOptions()
+op.add_argument("--headless=new")
+op.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
+op.add_argument("--no-sandbox")
+
+# Uses selenium to obtain stats from csgostats.gg
+def scrape_profile(player_profile, hyperlink, queue=None):
+    url = player_profile
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=op)
+    try:
+        matches = get_matches(driver,player_profile)
+        driver.get(url)
+        try:
+            meta_tag = driver.find_element(By.XPATH, "//meta[@property='og:description']")
+            meta_content = meta_tag.get_attribute("content")
+            # Find the <div> elements with class="total-stat"
+            total_stat_elements = driver.find_elements(By.CLASS_NAME, "total-stat")
+            total_games = total_stat_elements[0].find_element(By.CLASS_NAME, "total-value").text
+            return (hyperlink, meta_content, total_games, matches)
+            
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            if queue is None:
+                return (hyperlink, None, None, None)
+            else:
+                queue.put((hyperlink, None, None, None))
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+    finally:
+        if driver is not None:
+            driver.quit()
+
+def get_element(html, index):
+    return html[index].get_attribute('innerText')
+
+def scrap_row(content,row):
+    tds = row.find_elements(By.TAG_NAME,'td')
+    content['Date'].append(get_element(tds,0))
+    content['Map'].append(get_element(tds,2))
+    content['Score'].append(get_element(tds,3))
+    content['Kills'].append(int(get_element(tds,5)))
+    content['Deaths'].append(int(get_element(tds,6)))
+    content['Assists'].append(int(get_element(tds,7)))
+    content['HS'].append(int(get_element(tds,9)))
+    content['ADR'].append(int(get_element(tds,10)))
+    content['Rating'].append(float(get_element(tds,19)))
+
+def get_matches(driver : webdriver.Chrome,player_profile):
+    content = {'Date':[],'Map':[],'Score':[],'Kills':[],'Deaths':[],'Assists':[],'HS':[],'ADR':[],'Rating':[]}
+    driver.get(player_profile+'#/matches')
+    try:
+        rows = driver.find_elements(By.CLASS_NAME, "p-row.js-link")
+        for row in rows:
+            scrap_row(content,row)
+    except Exception as e:
+        print(f"Error: {str(e)}")
+    return content
+
+
+def get_stats(user_profile_hyper_link, meta_content, total_games, find_win_percentage_regex,
+              find_kill_death_ratio_regex, find_hltv_rating_regex, find_headshot_percentage_regex, find_adr_regex):
+    if meta_content is not None:
+        find_win_percentage = re.search(find_win_percentage_regex, str(meta_content))
+        find_kill_death_ratio = re.search(find_kill_death_ratio_regex, str(meta_content))
+        find_hltv_rating = re.search(find_hltv_rating_regex, str(meta_content))
+        find_headshot_percentage = re.search(find_headshot_percentage_regex, str(meta_content))
+        find_adr = re.search(find_adr_regex, str(meta_content))
+        win_percentage = find_win_percentage.group(1)
+        kill_death_ratio = find_kill_death_ratio.group(1)
+        hltv_rating = find_hltv_rating.group(1)
+        headshot_percentage = find_headshot_percentage.group(1)
+        adr = find_adr.group(1)
+
+        return (
+        user_profile_hyper_link, hltv_rating, kill_death_ratio, adr, win_percentage, headshot_percentage, total_games,
+    )
+    else:
+        return (user_profile_hyper_link, '0', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'NOG')
