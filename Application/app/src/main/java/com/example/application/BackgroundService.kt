433c35c9ed2959a71383b8f3372c7a4c661be9de
JoseBambora <zebragapt@gmail.com>
2024-05-01 16:32:28 +0100

    Fixed minor problem related with foreground service.

--- Application/app/src/main/java/com/example/application/BackgroundService.kt
+++ Application/app/src/main/java/com/example/application/BackgroundService.kt
@@ -1,11 +1,15 @@
 package com.example.application
 
 import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.example.application.utils.physicalactivity.ActivitySensorsHelper
 import com.example.application.utils.SaveData
 import com.example.application.utils.weather.WeatherSensorsHelper
@@ -38,9 +42,30 @@ class BackgroundService : Service() {
             pendingIntent
         )
     }
+    private fun createNotificationChannel() {
+        val CHANNEL_ID = "notifications"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Channel Name"
+            val descriptionText = "Channel Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Foreground Service")
+            .setContentText("Service is running")
+            .setSmallIcon(R.drawable.logo)
+            .build()
+        startForeground(100, notification)
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d("DebugApp","A começar serviço")
+        createNotificationChannel()
         activitySensorsHelper.onStart()
         weatherSensorsHelper.onStart()
         sleepSensorsHelper.onStart()
